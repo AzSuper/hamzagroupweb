95a36883298c13ba998036b42c98421f91d90a6c
AzSuper <mohammadabozamel6@gmail.com>
2025-03-04 22:38:50 +0300

    Initial commit of Next.js project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "off", // Disable the rule
       "@typescript-eslint/no-unused-vars":"off",
+      "prefer-const":"off",
     },
   },
 ];
