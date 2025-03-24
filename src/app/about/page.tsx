46efcba7c44431077345f60ade24885c333df501
AzSuper <117565948+AzSuper@users.noreply.github.com>
2025-03-24 12:05:04 +0300

    Update page.tsx
    include the "OrgChart" component into the main about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import AboutIntro from "@/components/AboutIntro"
 import Bref from "../../components/Bref";
-
+import OrgChart from "../../components/OrgChart"
 const Page = () => {
   return (
     <>
       <AboutIntro />
+      <OrgChart/>
       <Bref />
     </>
   );
